10184ea97d9817397e69af42ee0f3d9acb861450
Jayvee <jayveecastaneda19@gmail.com>
2021-07-15 15:30:36 +0800

    Edited names, titles and images

--- resources/db/data.js
+++ resources/db/data.js
@@ -5,22 +5,11 @@ const PLACEHOLDER_IMAGE = {
 
 const news = [
     {
-        title: "News item title",
-        date: "dummy", // date object in string form, for now, contains a dummy string
+        title: "UPV Komsai.Org Website Launch",
+        date: "July 2, 2021", // date object in string form, for now, contains a dummy string
         content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut suscipit velit, a vestibulum felis. Vestibulum vestibulum, turpis ut condimentum accumsan, mauris orci fringilla justo, et mattis nibh ipsum sed sem. Aenean ornare libero at lorem tempus ullamcorper. Nam ullamcorper cursus odio, vel consequat ligula sollicitudin quis. Phasellus quis erat sagittis risus bibendum aliquet id pretium nisi.`,
         img_path:
             "http://mediad.publicbroadcasting.net/p/vpr/files/styles/x_large/public/201808/stack-of-newspapers-istock-bernie_photo.jpg",
-    },
-];
-
-const skills = [
-    {
-        name: "Software Development",
-        img_path: "resources/img/skills/undraw_programming.svg",
-    },
-    {
-        name: "Web Development",
-        img_path: "resources/img/skills/undraw_Code_typing.svg",
     },
 ];
 
