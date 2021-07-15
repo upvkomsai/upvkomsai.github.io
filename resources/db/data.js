f22bb81ee0a02be8b410288a5313b78f4b70f96f
Stick1000 <janleymolina@outlook.com>
2021-07-15 15:04:19 +0800

    Added template code

---
+++ resources/db/data.js
@@ -0,0 +1,25 @@
+const PLACEHOLDER_IMAGE = {
+    NEWS: null,
+    PROJECTS_DONE: null,
+};
+
+const news = [
+    {
+        title: "News item title",
+        date: "dummy", // date object in string form, for now, contains a dummy string
+        content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut suscipit velit, a vestibulum felis. Vestibulum vestibulum, turpis ut condimentum accumsan, mauris orci fringilla justo, et mattis nibh ipsum sed sem. Aenean ornare libero at lorem tempus ullamcorper. Nam ullamcorper cursus odio, vel consequat ligula sollicitudin quis. Phasellus quis erat sagittis risus bibendum aliquet id pretium nisi.`,
+        img_path:
+            "http://mediad.publicbroadcasting.net/p/vpr/files/styles/x_large/public/201808/stack-of-newspapers-istock-bernie_photo.jpg",
+    },
+];
+
+const projectsDone = [
+    {
+        name: "Zest",
+        date_started: "Oct 2020",
+        date_completed: "Jun 2021",
+        description: "An innovative approach to organizing your day.",
+        img_path: "resources/img/projects/zest.png",
+        url: "https://github.com/jvc1019/Zest",
+    },
+];
