dc5253003003461bf839bfd8b95fa00020cb059d
Jayvee <jayveecastaneda19@gmail.com>
2021-07-15 20:17:51 +0800

    Updated content

--- resources/db/data.js
+++ resources/db/data.js
@@ -8,8 +8,19 @@ const news = [
         title: "UPV Komsai.Org Website Launch",
         date: "July 2, 2021", // date object in string form, for now, contains a dummy string
         content: "Love this new website OMG!",
-        img_path:
-            "http://mediad.publicbroadcasting.net/p/vpr/files/styles/x_large/public/201808/stack-of-newspapers-istock-bernie_photo.jpg",
+        img_path: "resources/img/Logo.png",
+    },
+    {
+        title: "Congratulations on Finishing the Semester!",
+        date: "July 2, 2021", // date object in string form, for now, contains a dummy string
+        content: "Love this new website OMG!",
+        img_path: "resources/img/news/Congrats_2ndSem.png",
+    },
+    {
+        title: "Congratulations Honorofic Scholars!!!",
+        date: "July 2, 2021", // date object in string form, for now, contains a dummy string
+        content: "Love this new website OMG!",
+        img_path: "resources/img/news/CAS_Honorific_Scholars.png",
     },
 ];
 
@@ -18,7 +29,7 @@ const projectsDone = [
         name: "Mockup Website",
         date_shown: "July 15 2021",
         description: "An innovative approach to organizing your day.",
-        img_path: "resources/img/projects/zest.png",
+        img_path: "resources/img/Logo.png",
         url: "https://github.com/jvc1019/Zest",
     },
 ];
