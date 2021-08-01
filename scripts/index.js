df633ba8af02b7ce4723b092c09e2683fffe929e
Ann <71485613+anneliszt@users.noreply.github.com>
2021-08-01 11:03:17 +0800

    Edited FAQs CSS

--- scripts/index.js
+++ scripts/index.js
@@ -509,3 +509,19 @@ const bindButtons = (buttons) => {
 };
 
 bindButtons(buttons);
+
+var acc = document.getElementsByClassName("question");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
