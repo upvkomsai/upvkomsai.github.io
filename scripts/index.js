6b8e886da25be7c4178f77d35f8740bdd99a748f
Ann <71485613+anneliszt@users.noreply.github.com>
2021-08-03 18:13:33 +0800

    Mobile navbar closes after clicking an option

--- scripts/index.js
+++ scripts/index.js
@@ -399,14 +399,21 @@ const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
 
+const navLink = document.querySelectorAll(".mainMenu");
+
 openMenu.addEventListener('click',show);
 closeMenu.addEventListener('click',close);
+navLink.forEach(n => n.addEventListener("click", autoClose));
 
 function show(){
     mainMenu.style.display = 'flex';
     mainMenu.style.top = '0';
 }
 function close(){
+    mainMenu.style.top = '-100%';
+}
+
+function autoClose() {
     mainMenu.style.top = '-100%';
 }
 
