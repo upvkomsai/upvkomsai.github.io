0fe04618139e6af684ecc186f3411d80b7d45955
Ann <71485613+anneliszt@users.noreply.github.com>
2021-08-05 15:10:18 +0800

    Added description for carousel

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,35 @@ const CARD = {
     SMALL: "card-sm",
 };
 
+// Carousel 
+
+var slideIndex = 1;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {slideIndex = 1}    
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" current", "");
+        }
+        slides[slideIndex-1].style.display = "block";  
+        dots[slideIndex-1].className += " current";
+    }
+
 /**
  * @param {string} layout - card layout as defined in the CARD constant (horizontal, vertical, or small)
  * @param {boolean} includeModal - bind a modal to the Card item to show more details
