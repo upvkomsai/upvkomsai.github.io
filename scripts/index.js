fe3863a6e91a7fe784f0296e3607985e6122fb15
Christian Dale Celestial <cpcelestial@up.edu.ph>
2021-08-06 19:49:53 +0800

    Banner changes
    Co-Authored-By: Ann <71485613+anneliszt@users.noreply.github.com>
    Co-Authored-By: Christian Dale Celestial <cpcelestial@up.edu.ph>

--- scripts/index.js
+++ scripts/index.js
@@ -10,31 +10,31 @@ const CARD = {
 // Carousel 
 
 var slideIndex = 1;
-    showSlides(slideIndex);
+showSlides(slideIndex);
 
-    function plusSlides(n) {
-        showSlides(slideIndex += n);
-    }
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 
-    function currentSlide(n) {
-        showSlides(slideIndex = n);
-    }
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
-    function showSlides(n) {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        var dots = document.getElementsByClassName("dot");
-        if (n > slides.length) {slideIndex = 1}    
-        if (n < 1) {slideIndex = slides.length}
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";  
-        }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" current", "");
-        }
-        slides[slideIndex-1].style.display = "block";  
-        dots[slideIndex-1].className += " current";
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
     }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" current", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " current";
+}
 
 /**
  * @param {string} layout - card layout as defined in the CARD constant (horizontal, vertical, or small)
